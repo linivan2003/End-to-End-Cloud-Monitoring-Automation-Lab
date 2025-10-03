22e0740a070f05ae244a04e5a4503dbb570e7792
Ivan Lin <linivan2003@gmail.com>
2025-10-02 20:41:14 -0700

    tutorial complete

--- main.tf
+++ main.tf
@@ -15,6 +15,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = var.resource_group_name
   location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
 }
